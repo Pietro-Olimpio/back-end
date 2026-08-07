8cac0a864db7b3cc9db853cfaa06020bb4e43c7b
Pietro-Olimpio <pietro.olimpio@edu.senai.br>
2026-08-07 10:49:32 -0300

    um simples index.php

--- PHPVanilla/HelloWorld/index.php
+++ PHPVanilla/HelloWorld/index.php
@@ -8,8 +8,15 @@
 <body>
     <h1>Olá Alunos!!! Vamos Aprender PHP???</h1>
     <?php 
-    echo "Hello, World!!!"
-    //sera exibito um paragrafo com o texto acima
+    // utilização de Tagm <?php "Aqui Vai o Código PHP" >
+    // para imprimir algo usamos o código "echo"
+    echo "Hello, World!!!";
+    // Sera Exibido um parágrafo com o texto acima
+    echo "Esse é um Código PHP que será exibido";
+    # esse é comentários, não será exibido
+    echo "Esse será exibido Tambem";
+    /* essa é outra forma de fazer comentário*/
+    // sempre fechar as linhas de comando PHP com ";" => para o PHP entender que o Comando foi Finalizado
     ?>
 
     
